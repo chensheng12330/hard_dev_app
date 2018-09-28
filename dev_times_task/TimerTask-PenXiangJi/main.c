f10ba6370036d483916f04d51eacde935183b94b
chenchengjian <535239186@qq.com>
2018-09-28 18:15:05 +0800

    1。完成基础的逻辑，待调试通过.

--- dev_times_task/TimerTask-PenXiangJi/main.c
+++ dev_times_task/TimerTask-PenXiangJi/main.c
@@ -37,11 +37,21 @@ L2(红io1)闪一次，表示时间较准完成。
 L2(红)闪二次，表示计数恢复完成。 
 L2(红)闪三次，表示关闭定时
 
+/*stc15f104w 引脚安排
+*********************************************************
+电机电源  p3.4  -- ||--p3.3  喷香机按键控制输入
+正电源       vcc    -- ||--p3.2  时间重置按键控制输入
+电机开关  p3.5  -- ||--p3.1  红灯
+负电源        gnd  -- ||--p3.0  绿灯
+*********************************************************
 
-******************************************/
+*/
+
+/******************************************/
 
 /*************	本地常量声明	**************/
-
+#define l2Num_TimeRest_1 1
+#define l2Num_MotoRun_2  2
 
 /*************	本地变量声明	**************/
 u32 g_hour=0;  //时
@@ -56,31 +66,27 @@ u8  g_light_on_time=1; //默认为20分钟. 单位为分钟
 
 
 sbit ioInKeyForTime = P3^2; //时间较准按键输入  int0
-sbit ioInKeyForRun  = P3^3;//程序是否运行输入   int1
-sbit ioInKeyForMoto = P3^5;//喷香机触发运行输入  int3
+//sbit ioInKeyForRun  = P3^3;//程序是否运行输入   int1
+sbit ioInKeyForMoto = P3^3;//喷香机触发运行输入  int1
 
-sbit ioOutForL1Green= P3^1;  //绿灯，程序运行指示灯
-sbit ioOutForL2Red  = P3^1;  //红灯，按键响应指示灯
-sbit ioOutForMotoPower=P3^4;  //喷香机电源开关IO口. 
-sbit ioOutForMotoKey= P3^0;  //喷香机驱动开关IO口. 
+sbit ioOutForL1Green= P3^0;  //绿灯，程序运行指示灯
+sbit ioOutForL2Red  = P3^1;  //红灯，按键响应指示灯 
+sbit ioOutForMotoPower=P3^4;  //喷香机电源开关IO口.
+sbit ioOutForMotoKey= P3^5;  //喷香机按键开关控制IO口. 
 
-u8 ioSwitchLED=0;
-u8 ioWorkLED=0;
-u8 ioKEY=0;
 
 #define  k_moto_run_time 10; //50 //定时叠加数
 u8 g_moto_run_time=0; //电机启动运行后的时间值
  
 typedef struct
 {
-	u8	sKeyForTime;  //时间键状态值.  0: 未按下  //1:已按下
-	u8	sKeyForRun;	  //程序键状态值.  0: 未按下  //1:已按下
-    u8  sKeyForMoto;  //喷香机键状态值.  0: 未按下  //1:已按下
+	u8	sKeyForTime;  //重置时间键状态值.  0: 未按下  //1:已按下
+	//u8	sKeyForRun;	  //程序键状态值.  0: 未按下  //1:已按下
+	u8     sKeyForMoto;  //喷香机键状态值.  0: 未按下  //1:已按下
 } KeyStateDef; //当前程序按键的状态,默认为0 
 
 //全局对象
-KeyStateDef  g_allKeyState={0,0,0};
-
+KeyStateDef  g_allKeyState={0,0};
 
 
 /*************	本地函数声明	**************/
@@ -113,11 +119,13 @@ void	EXTI_config(void)
 	EXTI_InitStructure.EXTI_Interrupt = ENABLE;				//中断允许,     ENABLE或DISABLE
 	Ext_Inilize(EXT_INT1,&EXTI_InitStructure);				//	EXT_INT0,EXT_INT1,EXT_INT2,EXT_INT3,EXT_INT4
 
+/*
     //初始化INT3
 	EXTI_InitStructure.EXTI_Mode      = EXT_MODE_Fall;	//中断模式,  	EXT_MODE_RiseFall, EXT_MODE_Fall
 	EXTI_InitStructure.EXTI_Polity    = PolityLow;			//中断优先级,   PolityLow,PolityHigh
 	EXTI_InitStructure.EXTI_Interrupt = ENABLE;				//中断允许,     ENABLE或DISABLE
 	Ext_Inilize(EXT_INT3,&EXTI_InitStructure);
+	*/
 }
 
 
@@ -126,13 +134,13 @@ void	EXTI_config(void)
 void	GPIO_config(void)
 {
 	GPIO_InitTypeDef	GPIO_InitStructure;				//结构定义
-	GPIO_InitStructure.Pin  = GPIO_Pin_0 | GPIO_Pin_1| GPIO_Pin_5;	//指定要初始化的IO, 
+	GPIO_InitStructure.Pin  = GPIO_Pin_0 | GPIO_Pin_1| GPIO_Pin_4|GPIO_Pin_5;	//指定要初始化的IO, 
 	// GPIO_Pin_2 ~ GPIO_Pin_3, 或操作
 	GPIO_InitStructure.Mode = GPIO_OUT_PP;				//指定IO的输入或输出方式,GPIO_PullUp,GPIO_HighZ,GPIO_OUT_OD,GPIO_OUT_PP
 	GPIO_Inilize(GPIO_P3,&GPIO_InitStructure);			//初始化
 
 
-	GPIO_InitStructure.Pin   = GPIO_Pin_2 |GPIO_Pin_3 | GPIO_Pin_4 ;	//指定要初始化的IO,
+	GPIO_InitStructure.Pin   = GPIO_Pin_2 |GPIO_Pin_3  ;	//指定要初始化的IO,
 	GPIO_InitStructure.Mode = GPIO_PullUp;				//指定IO的输入或输出方式,GPIO_PullUp,GPIO_HighZ,GPIO_OUT_OD,GPIO_OUT_PP
 	GPIO_Inilize(GPIO_P3,&GPIO_InitStructure);	
 	
@@ -164,15 +172,14 @@ void main(void)
 
     //全局变量初使化
 	// 工作灯开始
-      ioOutForL1Green=ioInKeyForMoto=ioInKeyForRun=ioInKeyForTime=1; //key键拉高
+      ioOutForL1Green=ioInKeyForMoto=ioInKeyForTime=1; //key键拉高
 
       //输出io脚先关闭
       ioOutForL2Red =ioOutForMotoPower = 0;
 
        ioOutForMotoKey=1; //触发喷香机的按键开关. 低位开，高位关.
 
-            
-
+           
         //g_allKeyState = {0,0,0};
 
 
@@ -198,7 +205,7 @@ void main(void)
 
 		if( g_hour ==7 || g_hour ==13 || g_hour ==19 ) {
 
- 		       //MotoStart();
+ 		       motoStart();
 				
 		}
 	}
@@ -210,13 +217,15 @@ void main(void)
 void key_scan(void) {
 
 	if(g_allKeyState.sKeyForMoto == 1) {
-
-		//触发喷香机工作
-
 		//清空原状态
 		g_allKeyState.sKeyForMoto = 0;
-	}
 
+		//触发喷香机工作
+		motoStart();
+                  l2ShowWithNum(l2Num_MotoRun_2);
+		
+	}
+/*
 	if(g_allKeyState.sKeyForRun== 1) {
 
 		//停止APP运行
@@ -225,15 +234,17 @@ void key_scan(void) {
 		//清空原状态
 		//g_allKeyState.sKeyForRun = 0;
 	}
-
+*/
 	if(g_allKeyState.sKeyForTime== 1) {
+		//清空原状态
+		g_allKeyState.sKeyForTime = 0;
 
 		//重置时间为12点.
 		g_hour = 12;
 		g_minute=g_second=g_millisecond =0;
 
-		//清空原状态
-		g_allKeyState.sKeyForTime = 0;
+        l2ShowWithNum(l2Num_TimeRest_1);
+		
 	}	
 }
 
@@ -244,13 +255,15 @@ void key_scan(void) {
 常亮表示电池不足。
 
 L2(红io1)闪一次，表示时间较准完成。
-L2(红)闪二次，表示计数恢复完成。 
-L2(红)闪三次，表示关闭定时
+L2(红)闪二次，表示触发喷香机工作。 
+L2(红)闪三次，表示关闭APP运行
 */ 
+
 void l2ShowWithNum(u8 num)
 {
-
-	int i=0;	
+	int i=0;
+    PrintString("\r\n l2ShowWithNum. ");
+		
 	for(i=0; i<num; i++)
 	{
 		ioOutForL2Red = 0;
@@ -268,17 +281,22 @@ void l2ShowWithNum(u8 num)
 void motoStart(void) {
     //设置电机启动标志时间值.
     g_moto_run_time = k_moto_run_time; 
+    PrintString("\r\n motoStart. ");
 }
 
 void secondAction(void) {
 	//PrintString("\r\n secondAction... ");
 	printNowTime();
     
-	if(g_second%10 == 0){
-		ioOutForMotoPower = 0;
-		delay_ms(200);
-		ioOutForMotoPower = 1;
+	if(g_second%10 == 0){   //工作指示灯(绿)
+		ioOutForL1Green = 1;
+		//delay_ms(200);
+		
 	}
+    else {
+            ioOutForL1Green = 0;
+        }
+
 
         if(g_moto_run_time==0){
                 return;
@@ -412,25 +430,25 @@ void INT1_int (void) interrupt INT1_VECTOR		//进中断时已经清除标志
 
     PrintString("\r\n 外部中断1.延时1秒");
     delay_ms(2000);
-	EX1 = 1;
-		;	  //程序键状态值.  0: 未按下  //1:已按下
+	EX1 = 1;	 ;	  //程序键状态值.  0: 未按下  //1:已按下
     
-	g_allKeyState.sKeyForRun= 1;
+	g_allKeyState.sKeyForMoto= 1;
 }
 
-/********************* INT3中断函数 ************************/
+/********************* INT3中断函数 ***********************
 void INT3_int  (void) interrupt INT3_VECTOR
 {
     PrintString("\r\n 外部中断3.延时1秒");
 
-/*
+//
     INT_CLKO |=  (1 << 5);  //允许中断  
     INT_CLKO &= ~(1 << 5);  //禁止中断
-*/
+///
 	//EX3 = 0;	
     //IE1  = 0;	
 
 	g_allKeyState.sKeyForMoto= 1;
 }
+*/
 
 
